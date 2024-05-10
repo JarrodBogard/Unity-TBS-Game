fee62141d00789ac5db99069e3fd707b6efab6a0
Jarrod Bogard <jarrodbogard@gmail.com>
2024-05-10 13:35:02 -0400

    refactor unit and unitactionsystem scripts

--- Assets/Scripts/MoveAction.cs
+++ Assets/Scripts/MoveAction.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class MoveAction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Animator unitAnimator;
+
+    private Vector3 targetPosition;
+
+    private void Awake()
     {
-        
+        targetPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float stoppingDistance = .1f;
+        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
+        {
+
+            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            float moveSpeed = 4f;
+            transform.position += moveDirection * moveSpeed * Time.deltaTime;
+            float rotateSpeed = 10f;
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+            unitAnimator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            unitAnimator.SetBool("IsWalking", false);
+        }
+    }
+
+    public void Move(Vector3 targetPosition)
+    {
+        this.targetPosition = targetPosition;
     }
 }
