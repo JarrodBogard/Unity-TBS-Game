c9e0757ee10e73f0b37cb7e43ec515ae80f43851
Jarrod Bogard <jarrodbogard@gmail.com>
2024-05-18 21:31:25 -0400

    add move action and spin action features

--- Assets/Scripts/MoveAction.cs
+++ Assets/Scripts/MoveAction.cs
@@ -10,6 +10,7 @@ public class MoveAction : MonoBehaviour
 
     private Unit unit;
     private Vector3 targetPosition;
+    private bool isActive;
 
     private void Awake()
     {
@@ -19,26 +20,30 @@ public class MoveAction : MonoBehaviour
 
     void Update()
     {
+        if (!isActive) { return; }
         float stoppingDistance = .1f;
+        Vector3 moveDirection = (targetPosition - transform.position).normalized;
         if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
         {
 
-            Vector3 moveDirection = (targetPosition - transform.position).normalized;
             float moveSpeed = 4f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
-            float rotateSpeed = 10f;
-            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
             unitAnimator.SetBool("IsWalking", true);
         }
         else
         {
             unitAnimator.SetBool("IsWalking", false);
+            isActive = false;
         }
+
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
     }
 
     public void Move(GridPosition gridPosition)
     {
         this.targetPosition = LevelGrid.Instance.GetWorldPosition(gridPosition);
+        isActive = true;
     }
 
     public bool IsValidActionGridPosition(GridPosition gridPosition)
